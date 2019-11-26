f6052098267927607ba0585920c5167c469439c3
zeraMe1ster <sauhard.eg@gmail.com>
2019-11-27 05:12:44 +0545

    Logic

--- app/src/main/java/com/example/application/MainActivity.java
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etMeter;
+    private Button btnConvert;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etMeter = findViewById(R.id.etMeter);
+        btnConvert = findViewById(R.id.btnConvert);
+        tvResult = findViewById(R.id.tvResult);
+
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Conversion logic
+
+                double multiplier = 39.37;
+                double result = 0.0;
+                double meterValue = Double.parseDouble(etMeter.getText().toString());
+
+                result = meterValue * multiplier;
+
+                tvResult.setText(Double.toString(result) + " inches");
+
+            }
+        });
+
+
     }
 }
